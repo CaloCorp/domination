c17007239adbc4141469b9d912f8794bff3358b3
Calo Corp <marc.calo@codeimmersives.com>
2020-02-11 15:48:29 -0500

    doing

---
+++ main.js
@@ -0,0 +1,19 @@
+function doesSomething (id,url) {
+
+const mything= document.querySelector ('#' +id)
+
+mything.src=url
+
+
+}
+
+doesSomething('image-1','https://media3.giphy.com/media/UMLFm3OcX0b6M/giphy.gif')
+
+doesSomething('image-2', 'https://media0.giphy.com/media/A8UFISckEbokw/giphy.gif' )
+
+doesSomething('image-3','https://media1.tenor.com/images/00796e6163c4e89ef845a1e496a49b45/tenor.gif?itemid=13330018')
+
+function crossOut (ul) {
+
+
+}
