35be44fa14673e5bad47e24762d9c735d91b3153
Calo Corp <marc.calo@codeimmersives.com>
2020-02-12 09:55:03 -0500

    working through

--- main.js
+++ main.js
@@ -13,7 +13,29 @@ doesSomething('image-2', 'https://media0.giphy.com/media/A8UFISckEbokw/giphy.gif
 
 doesSomething('image-3','https://media1.tenor.com/images/00796e6163c4e89ef845a1e496a49b45/tenor.gif?itemid=13330018')
 
-function crossOut (ul) {
+function crossOut () {
 
+const firstChildOfList= document.querySelector('#arguments').firstElementChild;
+firstChildOfList.style.textDecoration = 'line-through'
 
 }
+
+crossOut ()
+
+function removeLastChild () {
+
+const lastChildofList =document.querySelector('#arguments').lastElementChild;
+
+lastChildofList.remove()
+}
+
+removeLastChild ()
+
+removeLastChild ()
+
+
+
+
+
+
+
